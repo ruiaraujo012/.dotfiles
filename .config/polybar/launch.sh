a0c74942405d3689109020195ce6d7cd12e1102b
ruiaraujo012 <ruiaraujo012@gmail.com>
2020-02-19 01:39:00 +0000

    Update polybar theme

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# polybar -r example &
-
+# Launch bars
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload main &
   done
 else
-  polybar --reload example &
+  polybar --reload main &
 fi
 
 echo "Bars launched..."
